de1949c1de871c4c90516b5e7887d0f26acd8860
Thiago Pereira <ths.pereira@gmail.com>
2021-03-24 16:20:10 -0300

    Tipagem dos grapqhql

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,6 +3,7 @@ import { GET_PAGE_BY_SLUG, GET_PAGES } from 'graphql/queries'
 import PageTemplate, { PageTemplateProps } from 'templates/Pages'
 import { useRouter } from 'next/router'
 import { GetStaticProps } from 'next'
+import { GetPageBySlugQuery, GetPagesQuery } from '../graphql/generated/graphql'
 
 export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
@@ -13,8 +14,8 @@ export default function Page({ heading, body }: PageTemplateProps) {
   return <PageTemplate heading={heading} body={body} />
 }
 
-export const getStaticPaths = async () => {
-  const { pages } = await client.request(GET_PAGES, { first: 3 })
+export async function getStaticPaths() {
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
 
   const paths = pages.map(({ slug }) => ({
     params: { slug }
@@ -24,7 +25,7 @@ export const getStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
     slug: `${params?.slug}`
   })
 
