30f73213151cb2e9ad2af9ad244453a76f541a5b
Thiago Pereira <ths.pereira@gmail.com>
2021-03-24 23:48:19 -0300

    Alteração no componente do mapa
    attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -22,7 +22,6 @@ const MAPBOX_STYLEID = process.env.NEXT_PUBLIC_MAPBOX_STYLEID
 const CustomTileLayer = () => {
   return MAPBOX_API_KEY ? (
     <TileLayer
-      attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
     />
   ) : (
